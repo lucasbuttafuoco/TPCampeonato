1efe918d98eb73bf25c0fa5dba930bff41b2b68e
julianmainardis <julian.mainardis@hotmail.com>
2022-04-25 21:04:02 -0300

    Creacion de championship y relacion de entidades

--- src/main/java/org/example/classes/Referee.java
+++ src/main/java/org/example/classes/Referee.java
@@ -3,24 +3,24 @@ package org.example.classes;
 import java.time.LocalDate;
 
 public class Referee extends Person {
-    public Referee(String name, LocalDate birthDate, String typeDNI, String dNI, String nationality, int years) {
+    public Referee(String name, LocalDate birthDate, String typeDNI, String dNI, String nationality, int yearsOfService) {
         super(name, birthDate, typeDNI, dNI, nationality);
-        this.years = years;
+        this.yearsOfService = yearsOfService;
     }
 
-    private int years;
+    private int yearsOfService;
 
-    public int getYears() {
-        return years;
+    public int getyearsOfService() {
+        return yearsOfService;
     }
 
-    public void setYears(int years) {
-        this.years = years;
+    public void setyearsOfService(int yearsOfService) {
+        this.yearsOfService = yearsOfService;
     }
 
     @Override
     public String toString() {
-        return super.toString() + " Referee: [Years on service:" + years +"] ";
+        return super.toString() + " Referee: [years on service:" + yearsOfService +"] ";
     }
 
 
