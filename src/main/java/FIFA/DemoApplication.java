7b6efd053d406be787ae8b4c257b37f44ae8d3b6
julianmainardis <julian.mainardis@hotmail.com>
2022-04-28 20:33:55 -0300

    Refactor mvc

--- src/main/java/FIFA/DemoApplication.java
+++ src/main/java/FIFA/DemoApplication.java
@@ -1,4 +1,4 @@
-package org.example;
+package FIFA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
